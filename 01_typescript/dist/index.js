4fe9cc6b9658c17ee18e5c78124fc85c24bd3d1a
Ivo Kostovski <ivo.kostovski@gmail.com>
2026-05-06 18:36:19 +0200

    Add initial TypeScript setup with basic types and configuration files

---
+++ 01_typescript/dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+// Basic types
+// String
+let title = 'Hello World, welcome to TypeScript learning';
+// title = 20
+title = typeof 20 === 'number' ? '20' : '';
+// Symbol
+const tag = Symbol('userId');
+// Number
+let count = 52;
+// count = 'jdasdas'
+let bigNumber = 9007199254740993n;
+// Boolean
+let isActive = false;
+// isActive = 'dasda'
+// Null
+let nothing = null;
+// Undefined
+let missing = undefined;
+//# sourceMappingURL=index.js.map
---
+++ 01_typescript/src/index.ts
@@ -0,0 +1,24 @@
+// Basic types
+// String
+let title: string = 'Hello World, welcome to TypeScript learning';
+// title = 20
+title = typeof 20 === 'number' ? '20' : '';
+
+// Symbol
+const tag: symbol = Symbol('userId');
+
+// Number
+let count: number = 52;
+// count = 'jdasdas'
+
+let bigNumber: bigint = 9_007_199_254_740_993n;
+
+// Boolean
+let isActive: boolean = false;
+// isActive = 'dasda'
+
+// Null
+let nothing: null = null;
+
+// Undefined
+let missing: undefined = undefined;
